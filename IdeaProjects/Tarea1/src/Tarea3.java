7e371555a08a9aa504d52fc92522d27e44cdd122
Jubal_Bernal <318044213@pcpuma.acatlan.unam.mx>
2023-02-22 23:29:37 -0600

    Se suben los archivos al repo

---
+++ IdeaProjects/Tarea1/src/Tarea3.java
@@ -0,0 +1,13 @@
+public class Tarea3 {
+    public static void main(String[] args){
+        // Este programa representa un marco del juego Gato
+
+
+        System.out.println("║═══║═══║═══║");
+        System.out.println("║═══║═══║═══║");
+        System.out.println("║═══║═══║═══║");
+        System.out.println("║═══║═══║═══║");
+
+    }
+
+}
